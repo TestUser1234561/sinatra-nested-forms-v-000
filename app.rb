51b610aed26784d3c3e91afeb0d84a9b375eec0f
Morgan Fudge <mfudge92@gmail.com>
2018-01-22 07:40:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ module FormsLab
             erb :'pirates/new'
         end
 
-        post 'pirates' do
+        post '/pirates' do
             @pirate = Pirate.new(params['pirate']['name'], params['pirate']['weight'], params['pirate']['height'])
             params['pirate']['ships'].each do |ship|
                 Ship.new(ship['name'], ship['type'], ship['booty'])
