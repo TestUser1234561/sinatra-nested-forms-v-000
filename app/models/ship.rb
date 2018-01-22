b1355f823d09b514980ed477e221fce6b2fa740b
Morgan Fudge <mfudge92@gmail.com>
2018-01-22 07:23:11 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @all = []
+
+    def initialize(name, type, booty)
+        @name = name
+        @height = type
+        @weight = booty
+        self.class.all << self
+    end
+
+    def self.clear
+        @all = []
+    end
 end
